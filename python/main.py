8624e1fb7f961d660b482c746065f45b3dc040fa
Chris Warren <chrisw@liatrio.com>
2022-05-24 10:29:05 -0500

    feat: add python vulnerability example

--- python/main.py
+++ python/main.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from flask_restful import Api, Resource
-  
-app =   Flask(__name__)
-  
-api =   Api(app)
-  
+
+import traceback
+
+app = Flask(__name__)
+
+api = Api(app)
+
 class HelloWorld(Resource):
-    def get(self):
-        data={"data":"Hello World"}
-        return data
-  
+  def get(self):
+    try:
+      data={"data":"Hello World"}
+      if (data != {"data":"Hello World"}):
+        raise Exception("")
+      return data
+    except Exception as e:
+      return traceback.format_exc()
+
 api.add_resource(HelloWorld,'/hello')
-  
-  
+
 if __name__=='__main__':
-    app.run(debug=False)
+  app.run(debug=False)
