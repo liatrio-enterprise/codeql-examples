bd29e73d2ab5474c88898a0ee0cb7045a2369167
wai-calvin <calvinw@liatrio.com>
2022-05-25 11:23:45 -0700

    feat: working example for python

--- python/app.py
+++ python/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, make_response, escape
+
 app = Flask(__name__)
 
 @app.route('/students/<id>', methods=['GET'])
 def student_id(id):
-   return f'student with id: ${id}'
+   student_id = request.args.get('id', '')
+   return make_response("Student id: " + escape(student_id))
 
-if __name__ == '__main__':
-   app.run(debug = True)
+# if __name__ == '__main__':
+#    app.run(debug = True)
