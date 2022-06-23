f0ca8dda7db2b03b1104aaf27d2f3a1cfd40b6d9
Michael Parker <michael@parker.gg>
2022-06-23 13:50:29 -0500

    feat: add POST endpoint for java example

--- java/src/main/java/com/liatrio/demo/StudentController.java
+++ java/src/main/java/com/liatrio/demo/StudentController.java
@@ -2,11 +2,10 @@ package com.liatrio.demo;
 
 import com.liatrio.demo.dao.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,11 @@ public class StudentController {
     public Student getById(@PathVariable("id") Long id) {
         System.out.println(id);
         return jdbcTemplate.queryForObject("SELECT * FROM Students WHERE id = ?", BeanPropertyRowMapper.newInstance(Student.class), id);
+    }
+
+    @PostMapping("/students")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createStudent(@RequestBody Student student) {
+        jdbcTemplate.update("INSERT INTO Students (name, age) VALUES (?, ?)", student.getName(), student.getAge());
     }
 }
